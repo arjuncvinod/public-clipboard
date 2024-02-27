e883a23524d09162bb2526815d173661290f743b
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-28 00:54:37 +0530

    nav deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 import { db } from "./firebase";
 import {
   onSnapshot,
